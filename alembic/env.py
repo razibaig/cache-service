848ddd12f2bf5c8c29ae7cf76b9dd202ac3ac4bb
Raziullah Baig <38754771+razibaig@users.noreply.github.com>
2025-02-01 10:12:23 +0500

    added alembic config for migrations

---
+++ alembic/env.py
@@ -0,0 +1,41 @@
+# alembic/env.py
+
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# Import your models' Base
+from app.models.payload_model import Base  # Adjust the import as necessary
+target_metadata = Base.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+run_migrations_online()
